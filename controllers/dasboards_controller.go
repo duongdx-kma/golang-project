71e983f11163153985076e034dd4429dd21dd7b3
duongdx <dinh.xuan.duong@sun-asterisk.com>
2024-01-13 23:16:36 +0700

    Duongdx - edit dashboard api

--- controllers/dasboards_controller.go
+++ controllers/dasboards_controller.go
@@ -11,6 +11,13 @@ func Hello(c echo.Context) error {
 	user := c.Get("user")
 
 	log.Println(user)
+	type response struct {
+		User    interface{} `json:"user"`
+		Message string      `json:"message"`
+	}
 
-	return c.JSON(http.StatusOK, user)
+	return c.JSON(http.StatusOK, response{
+		User:    user,
+		Message: "hello world ! 2024/01 ---",
+	})
 }
