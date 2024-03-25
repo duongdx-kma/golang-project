01e0e5f3aeef723285b33e8fa68ad0f571862ca9
duongdx <dinh.xuan.duong@sun-asterisk.com>
2024-03-26 00:39:17 +0700

    edit docker compose file

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '${APP_PORT} ${PORTAINER_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
+envsubst '${APP_PORT} ${PORTAINER_PORT} ${MINIO_PORT} ${MINIO_CONSOLE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 exec "$@"
